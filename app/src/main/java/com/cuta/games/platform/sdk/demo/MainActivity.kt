7b83d2829d017112faa172a6ae7a6fe6cf73afc3
swepthong <swepthong@126.com>
2025-04-11 10:36:37 +0800

    init project

---
+++ app/src/main/java/com/cuta/games/platform/sdk/demo/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.cuta.games.platform.sdk.demo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.cuta.games.platform.sdk.demo.ui.theme.CutaSdkTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CutaSdkTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    CutaSdkTheme {
+        Greeting("Android")
+    }
+}
---
+++ cuta-sdk/build.gradle.kts
@@ -0,0 +1,92 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.maven.publish)
+}
+
+android {
+    namespace = "com.cuta.games.platform.sdk"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 24
+
+        version = "0.0.1"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
+
+afterEvaluate {
+    val isAndroidLibraryProject = plugins.hasPlugin("com.android.library")
+    if (isAndroidLibraryProject) {
+        tasks.register<Copy>("copyDeps") {
+            from(configurations["includeJars"].filter { it.name.endsWith(".jar") })
+            into("./build/intermediates/packaged-classes/release/libs")
+        }
+        tasks.named("mergeReleaseJniLibFolders") {
+            dependsOn("copyDeps")
+        }
+
+        tasks.register<Copy>("copyDebugDeps") {
+            from(configurations["includeJars"].filter { it.name.endsWith(".jar") })
+            into("./build/intermediates/packaged-classes/debug/libs")
+        }
+        tasks.named("mergeDebugJniLibFolders") {
+            dependsOn("copyDebugDeps")
+        }
+    }
+
+    publishing {
+        publications {
+            create<MavenPublication>("library") {
+                groupId = "com.cuta.games.platform"
+                artifactId = "cutasdk"
+                version = "0.0.1"
+                artifact(tasks.named("bundleReleaseAar"))
+
+                pom.withXml {
+                    val dependenciesNode = asNode().appendNode("dependencies")
+                    configurations["includeJars"].allDependencies.forEach { dep ->
+                        val dependencyNode = dependenciesNode.appendNode("dependency")
+                        dependencyNode.appendNode("groupId", dep.group)
+                        dependencyNode.appendNode("artifactId", dep.name)
+                        dependencyNode.appendNode("version", dep.version)
+                        dependencyNode.appendNode("scope", "runtime")
+                    }
+                }
+            }
+        }
+        repositories {
+            mavenLocal()
+        }
+    }
+}
