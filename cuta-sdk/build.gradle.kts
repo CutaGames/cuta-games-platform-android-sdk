5fb73f616af036a585b7b84ea671ee40cf154997
swepthong <swepthong@126.com>
2025-06-10 16:28:53 +0800

    Prepare for release 0.0.1-alpha03

--- cuta-sdk/build.gradle.kts
+++ cuta-sdk/build.gradle.kts
@@ -53,7 +53,7 @@ afterEvaluate {
             register<MavenPublication>("release") {
                 groupId = "com.github.CutaGames"
                 artifactId = "cuta-games-platform-android-sdk"
-                version = "0.0.1-alpha02"
+                version = "0.0.1-alpha03"
 
                 artifact("$buildDir/outputs/aar/${project.name}-release.aar")
 
